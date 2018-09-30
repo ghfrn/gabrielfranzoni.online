b4f461320b7f409ede1d777ee7a6953b49967d55
Gyn <gyn@opmbx.org>
2018-09-30 03:11:24 +0000

    general improvements

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.get('/demos/fancy', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'fancy.html'));
+});
+
+app.get('/demos/epic', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'epic.html'));
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port);
